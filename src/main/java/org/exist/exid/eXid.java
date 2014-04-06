13ee7d17ae7a4fc7449fa0bab8b3bf34fe175034
Evgeny <gazdovsky@gmail.com>
2014-04-06 17:00:10 +0400

    add maven repo

--- src/main/java/org/exist/exid/eXid.java
+++ src/main/java/org/exist/exid/eXid.java
@@ -33,7 +33,6 @@ public class eXid {
     private static final char[] abc =  {
         '0', '1', '2', '3', '4', '5', '6', '7', '8', '9',
         'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
-        'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'
     };
 
     private static BigInteger l = BigInteger.valueOf(abc.length);
@@ -52,6 +51,10 @@ public class eXid {
         b.putLong(id.getLeastSignificantBits());
         BigInteger a = new BigInteger(1, b.array());
         return 'x' + translate(a, "");
+    }
+
+    public static void main(String[] arg) {
+        System.out.println(get());
     }
 
 }
